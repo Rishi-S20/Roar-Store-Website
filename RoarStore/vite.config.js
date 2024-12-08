5bf93d81c01fe647e4654230c9164bc69d71e54d
Rishi-S20 <rishisuryavanshi14@gmail.com>
2024-12-08 03:06:51 -0600

    Update vite.config.js

--- RoarStore/vite.config.js
+++ RoarStore/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: 'https://github.com/Rishi-S20/Roar-Store-Website.git',
+  base: '/Roar-Store-Website/',
   plugins: [react()],
 })
