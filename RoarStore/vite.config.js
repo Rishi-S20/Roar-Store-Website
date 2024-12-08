3eb41ad9c77025c32e07f049798ea2b3dd934d09
Rishi-S20 <rishisuryavanshi14@gmail.com>
2024-12-08 02:58:06 -0600

    deploying code

--- RoarStore/vite.config.js
+++ RoarStore/vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: 'https://github.com/Rishi-S20/Roar-Store-Website.git',
   plugins: [react()],
 })
